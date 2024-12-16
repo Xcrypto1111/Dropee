bb101ba1b220f4d1b5684d6db4322e0c43904cb9
Xcrypto1111 <xcrypto182@gmail.com>
2024-12-16 08:33:27 +0530

    Update logo.js

--- src/logo.js
+++ src/logo.js
@@ -1,5 +1,5 @@
 const printLogo = () => {
-  console.log(`Script By (https://t.me/D4rkCipherX)`.yellow);
+  console.log(`Script By (https://t.me/xcrypto1111)`.yellow);
 };
 
 module.exports = printLogo;
